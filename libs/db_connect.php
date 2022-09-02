5ce322b359e006d467c8bb7b668b2f3415240a2e
OrcaTech360 <112700018+OrcaTech360@users.noreply.github.com>
2022-09-02 17:20:43 +0530

    SchoolManagement

---
+++ libs/db_connect.php
@@ -0,0 +1,8 @@
+<?php
+$con=mysqli_connect("localhost","u310702159_growcapital360","Mayank@123","u310702159_school");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+ ?>
